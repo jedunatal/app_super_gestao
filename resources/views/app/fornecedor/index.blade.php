a8dffc62ce6de136a52ec1dd1abbae820589ef45
Jorge Eduardo <wyd.jrg@gmail.com>
2026-01-10 17:49:58 -0300

    incluindo comentário em bloco php puro

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,15 @@
+<h3>Fornecedor</h3>
+{{ 'Textos' }}
+
+<?= 'Texto de teste' ?>
+{{-- Comentário descartado pelo interpretador do blade --}}
+
+@php
+    // Para Comentários de uma linha
+
+    /*
+        Para comentários de multiplas linhas
+    */
+
+        echo 'Texxto';
+@endphp
